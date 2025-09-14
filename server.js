c951590719e1e52fbd3ef59759356ed3e751c605
Saravanan V <69112050+saravanan-vjd@users.noreply.github.com>
2025-09-14 21:48:04 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
+const authRoutes = require("./routes/auth");
+const chatRoutes = require("./routes/chats");
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+// Use clear namespaces
+app.use("/api/auth", authRoutes);   // -> /api/auth/signup, /api/auth/login
+app.use("/api/chats", chatRoutes);  // -> /api/chats/startChat, /api/chats/sendMessage, etc
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Backend running on port ${PORT}`));
